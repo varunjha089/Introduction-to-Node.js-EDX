01732fa799a3705d4f4830e916095c844810d33e
Varun Kumar <varunjha089@gmail.com>
2021-07-06 13:46:00 +0000

    Mocking GET Routes

---
+++ chapter-02/static-3/www/app.js
@@ -0,0 +1,34 @@
+const API = 'http://localhost:3000'
+
+const populateProducts = async () => {
+  const products = document.querySelector('#products')
+  products.innerHTML = ''
+
+  // Instead of using the client-side data we are using the another server for this purpose. 
+  const res = await fetch(API)
+  const data = await res.json()
+  // THE END
+
+  for (const product of data) {
+    const item = document.createElement('product-item')
+    for (const key of ['name', 'rrp', 'info']) {
+      const span = document.createElement('span')
+      span.slot = key
+      span.textContent = product[key]
+      item.appendChild(span)
+    }
+    products.appendChild(item)
+  }
+}
+
+document.querySelector('#fetch').addEventListener('click', async () => {
+  await populateProducts()
+})
+
+customElements.define('product-item', class Item extends HTMLElement {
+  constructor() {
+    super()
+    const itemTmpl = document.querySelector('#item').content
+    this.attachShadow({mode: 'open'}).appendChild(itemTmpl.cloneNode(true))
+  }
+})
